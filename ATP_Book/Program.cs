b53a62ff5d80343f0592a6890ea22d03505f22b9
spouk <spouk@DESKTOP-1F1DFF9>
2021-10-08 16:20:56 +0300

    Add project files.

---
+++ ATP_Book/Program.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace ATP_Book
+{
+    class Program
+    {
+        private static IWebDriver _driver;
+
+        [SetUp]
+        public static void Initialize()
+        {
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl("https://demos.bellatrix.solutions/");
+        }
+
+        [TearDown]
+        public static void TearDown()
+        {
+            _driver.Quit();
+        }
+
+        [Test]
+        public static void SuccessfullyOrdered_When_AddToCartButtonClicked()
+        {
+            var Falcon9AddToCartButton = _driver.FindElement(By.Id("28"));
+            var ViewCartButton = _driver.FindElement(By.ClassName("added_to_cart wc-forward"));
+
+            Falcon9AddToCartButton.Click();
+            ViewCartButton.Click();
+        }
+        
+    }
+}
